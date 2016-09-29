ebbbeac328ba7d696aa2df367bdfe6531bc9c8bf
schuylerwilliams <sky@Sky-MacBook-Air.local>
2016-09-29 10:26:43 -0500

    lotsa style

--- js2/script.js
+++ js2/script.js
@@ -1,9 +1,21 @@
 
 
+
+
 function changeMenuHover(source) {
-	document.getElementById(source).style.color = "#5DADE2";
+	document.getElementById(source).style.color = "#154360";
 }
 
 function changeMenuOut(source) {
 	document.getElementById(source).style.color = "";
+}
+
+function changeMenuClick(source) {
+	if (source == "menuItem01") {
+		
+	} else if (source == "menuItem02") {
+		
+	} else if (source == "menuItem03") {
+		
+	}
 }
