051c64851978edb46c10ad7cd883b85d530ac854
schuylerwilliams <schuylerwilliams@Sky-Pro.local>
2016-09-28 23:13:34 -0500

    quick update

---
+++ js2/script.js
@@ -0,0 +1,9 @@
+
+
+function changeMenuHover(source) {
+	document.getElementById(source).style.color = "#5DADE2";
+}
+
+function changeMenuOut(source) {
+	document.getElementById(source).style.color = "";
+}
