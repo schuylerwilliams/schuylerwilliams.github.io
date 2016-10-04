bdbc16445f250d2ebdd989f6b19be178354e15f8
schuylerwilliams <sky@Sky-MacBook-Air.local>
2016-10-03 23:54:35 -0500

    update carousel

--- mywork/js2/script.js
+++ mywork/js2/script.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
   var owl = $("#owl-demo");
  
   owl.owlCarousel({
-      items : 3, //3 items above 1000px browser width
-      itemsDesktop : [1000,3], //3 items between 1000px and 901px
-      itemsDesktopSmall : [900,3], // betweem 900px and 601px
-      itemsTablet: [600,2], //2 items between 600 and 0
+      items : 2, //3 items above 1000px browser width
+      itemsDesktop : [1000,2], //3 items between 1000px and 901px
+      itemsDesktopSmall : [900,2], // betweem 900px and 601px
+      itemsTablet: [600,1], //2 items between 600 and 0
       itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
   });
   
