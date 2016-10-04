a3b1f731b6e82228a5cd25ba1b204dcf6151fc6c
schuylerwilliams <schuylerwilliams@Sky-Pro.local>
2016-10-04 17:48:49 -0500

    updates

--- contact/index.php
+++ contact/index.php
@@ -1,3 +1,44 @@
+
+
+<?php
+	if (isset($_POST["submit"])) {
+		$personName = $_POST['personName'];
+		$email = $_POST['email'];
+		$userMessage = $_POST['userMessage'];
+		$from = 'Demo Contact Form'; 
+		$to = 'example@bootstrapbay.com'; 
+		$subject = 'Message from Contact Demo ';
+		
+		$body = "From: $personName\n E-Mail: $email\n Message:\n $userMessage";
+ 
+		// Check if name has been entered
+		if (!$_POST['personName']) {
+			$errName = 'Please enter your name';
+		}
+		
+		// Check if email has been entered and is valid
+		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+			$errEmail = 'Please enter a valid email address';
+		}
+		
+		//Check if message has been entered
+		if (!$_POST['userMessage']) {
+			$errMessage = 'Please enter your message';
+		}
+ 
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errMessage) {
+	if (mail ($to, $subject, $body, $from)) {
+		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+	} else {
+		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again.</div>';
+	}
+}
+	}
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
