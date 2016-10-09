f78401d47e3c39246e4dbbdcd23c7204ac38a9fc
schuylerwilliams <schuylerwilliams@Sky-Pro.local>
2016-10-09 17:17:49 -0500

    update work page

--- mywork/js2/script.js
+++ mywork/js2/script.js
@@ -21,11 +21,13 @@ $(document).ready(function() {
   
 	$('.link').on('click', function(event){
     	var $this = $(this);
+    	/*
     	if($this.hasClass('clicked')){
       	$this.removeAttr('style').removeClass('clicked');
     	} else{
       	$this.css('background','#7fc242').addClass('clicked');
     	}
+    	*/
   	});
   
   $('[data-toggle="tooltip"]').tooltip();
