75234ce3177380fe3e0a953b5f8f4361fdc6e146
Schuyler Williams <skywilliams@gmail.com>
2017-03-08 17:02:38 -0600

    update

--- js2/script.js
+++ js2/script.js
@@ -12,11 +12,11 @@ function changeMenuOut(source) {
 
 function changeMenuClick(source) {
 	if (source == "menuItem01") {
-		window.open("http://skywilliams.site/mywork/", "_self");
+		window.open("/mywork/", "_self");
 	} else if (source == "menuItem02") {
-		window.open("http://skywilliams.site/resume/", "_self");
+		window.open("/resume/", "_self");
 	} else if (source == "menuItem03") {
-		window.open("http://skywilliams.site/contact/", "_self");
+		window.open("/contact/", "_self");
 	}
 }
 
