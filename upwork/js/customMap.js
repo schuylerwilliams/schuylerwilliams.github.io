7df3d1b41d34075282b41c46824e49790f57c4bc
schuylerwilliams <sky@Sky-MacBook-Air.local>
2017-10-24 20:25:52 -0500

    update

--- upwork/js/customMap.js
+++ upwork/js/customMap.js
@@ -20,6 +20,14 @@ function initMap() {
     	fullscreenControl: false
 	});
 	
+	var bermudaTriangle = new google.maps.Polygon({
+    	paths: '../data/elementary_boundaries.geojson',
+    	strokeColor: '#FF0000',
+    	strokeOpacity: 0.8,
+    	strokeWeight: 2,
+    	fillColor: '#FF0000',
+    	fillOpacity: 0.35
+  	});
 	map.data.loadGeoJson('../data/elementary_boundaries.geojson');
 	map.data.loadGeoJson('../data/schools_v2.geojson');
 	
