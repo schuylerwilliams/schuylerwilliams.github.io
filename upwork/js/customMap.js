18211f528167ec0fbb2e960817c54983478f0267
schuylerwilliams <sky@Sky-MacBook-Air.local>
2017-10-24 20:21:10 -0500

    update

--- upwork/js/customMap.js
+++ upwork/js/customMap.js
@@ -4,7 +4,7 @@ var map;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
     	center: {lat: 37, lng: -93},
-    	zoom: 8,
+    	zoom: 15,
     	zoomControl: true,
     	zoomControlOptions: {
     		position: google.maps.ControlPosition.RIGHT_TOP
@@ -22,13 +22,6 @@ function initMap() {
 	
 	map.data.loadGeoJson('../data/elementary_boundaries.geojson');
 	map.data.loadGeoJson('../data/schools_v2.geojson');
-	
-	map.data.setStyle({
-		fillColor: 'green',
-		fillOpacity: 0.5,
-		strokeColor: 'green',
-		strokeOpacity: 1
-	});
 	
 };
 
