3e9ce6fa904b0bbbf0b1fe5d6722bda954a2027c
schuylerwilliams <sky@Sky-MacBook-Air.local>
2018-07-24 20:52:58 -0500

    vglawncare

---
+++ vglawncare/js/script.js
@@ -0,0 +1,22 @@
+
+var OpenDoorWellness = (function () {
+
+	var thisIsAVariableOrFunction = "true";
+	
+	$('.owl-carousel').owlCarousel({
+		loop:true,
+		margin:10,
+		nav:true,
+		items:1
+	});
+	
+	return {
+		thisIsAVariableOrFunction: thisIsAVariableOrFunction,
+	};
+
+})();
+
+
+$(document).ready(function(){
+	console.log(OpenDoorWellness.thisIsAVariableOrFunction);
+});
